135172954cac33b9132be43d32e040ee3b2a53ce
conor <conor.klamann@pennmedicine.upenn.edu>
2019-11-26 10:45:02 -0500

    fix footer styling

--- Site/webapp/wdkCustomization/js/client/main.ts
+++ Site/webapp/wdkCustomization/js/client/main.ts
@@ -1,31 +1,31 @@
 // import CSS/LESS/SAS files
-import 'site/wdkCustomization/css/client.css'; // copied from ApiCommonWebsite/Site/webapp/wdkCustomization/css/client.ss; may not be needed in long run
-import '../../sass/style.scss';
+import "../../css/client.css"; // copied from ApiCommonWebsite/Site/webapp/wdkCustomization/css/client.ss; may not be needed in long run
+import "../../sass/style.scss";
 
-import * as siteConfig from './config';
+import * as siteConfig from "./config";
 
 // initialize from ebrc
-import { initialize } from 'ebrc-client/bootstrap';
+import { initialize } from "ebrc-client/bootstrap";
 
 // import wrappers and additional routes
-import * as componentWrappers from './component-wrappers';
-import { wrapRoutes } from './routes';
+import * as componentWrappers from "./component-wrappers";
+import { wrapRoutes } from "./routes";
 
 // Initialize the application.
 const castInitialize: any = initialize;
 
 const context = castInitialize({
-	isPartOfEuPathDB: false,
-	includeQueryGrid: true,
-	componentWrappers,
-	wrapRoutes,
-	//pluginConfig
-	//storeWrappers,
+  isPartOfEuPathDB: false,
+  includeQueryGrid: true,
+  componentWrappers,
+  wrapRoutes
+  //pluginConfig
+  //storeWrappers,
 });
 
 //override default ebrc siteConfig with local
-const SITE_CONFIG_LOADED = 'eupathdb/site-config-loaded'
+const SITE_CONFIG_LOADED = "eupathdb/site-config-loaded";
 context.store.dispatch({
-	type: SITE_CONFIG_LOADED,
-	payload: { siteConfig }
-})
+  type: SITE_CONFIG_LOADED,
+  payload: { siteConfig }
+});
