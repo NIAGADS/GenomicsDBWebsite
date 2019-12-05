4b57544e9a642d6c4025edc131cebfc41038b64e
fossilfriend <egreenfest@gmail.com>
2019-12-05 15:05:53 -0500

    override Ebrc question form

--- Site/webapp/wdkCustomization/js/client/pluginConfig.js
+++ Site/webapp/wdkCustomization/js/client/pluginConfig.js
@@ -1,31 +1,35 @@
-import { 
-    // ResultTableSummaryViewPlugin,
-    StepAnalysisPathwayEnrichmentResults
-  } from 'wdk-client/Plugins';
-  
-  import { StepAnalysisGoEnrichmentResults } from './components/StepAnalysis/StepAnalysisGoEnrichmentResults';
-  
-  export default [
+import { StepAnalysisDefaultResult } from 'wdk-client/Plugins';
 
-   /* {
-      type: 'questionFilter',
-      name: 'matched_transcript_filter_array',
-      component: MatchedTranscriptsFilterPlugin
-    },
-    {
-      type: 'questionFilter',
-      name: 'gene_boolean_filter_array',
-      component: MatchedTranscriptsFilterPlugin
-    },*/
-   
-    {
-      type: 'stepAnalysisResult',
-      name: 'pathway-enrichment',
-      component: StepAnalysisPathwayEnrichmentResults
-    },
-    {
-      type: 'stepAnalysisResult',
-      name: 'go-enrichment',
-      component: StepAnalysisGoEnrichmentResults
-    },
-  ];
+import DefaultQuestionForm from 'wdk-client/Views/Question/DefaultQuestionForm';
+
+import { StepAnalysisGoEnrichmentResults } from './components/StepAnalysis/StepAnalysisGoEnrichmentResults';
+
+export default [
+
+  /* {
+     type: 'questionFilter',
+     name: 'matched_transcript_filter_array',
+     component: MatchedTranscriptsFilterPlugin
+   },
+   {
+     type: 'questionFilter',
+     name: 'gene_boolean_filter_array',
+     component: MatchedTranscriptsFilterPlugin
+   },*/
+
+  {
+    type: 'questionForm',
+    component: DefaultQuestionForm
+  },
+
+  {
+    type: 'stepAnalysisResult',
+    name: 'pathway-enrichment',
+    component: StepAnalysisDefaultResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'go-enrichment',
+    component: StepAnalysisGoEnrichmentResults
+  },
+];
