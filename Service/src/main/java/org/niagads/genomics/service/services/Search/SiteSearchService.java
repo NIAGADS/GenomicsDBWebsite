3a6b42b2dffdfdc35cc887579302b363a4551e3f
fossilfriend <egreenfest@gmail.com>
2019-12-17 14:26:39 -0500

    add descriptive term and fix displays for search result payload

--- Service/src/main/java/org/niagads/genomics/service/services/Search/SiteSearchService.java
+++ Service/src/main/java/org/niagads/genomics/service/services/Search/SiteSearchService.java
@@ -33,6 +33,13 @@ public class SiteSearchService extends AbstractWdkService {
     + "WHEN gene_symbol @@ to_tsquery((SELECT term FROM st) || ':*') THEN gene_symbol" + NL
     + "WHEN (annotation->>'name')::text @@ to_tsquery((SELECT term FROM st) || ':*') THEN (annotation->>'name')::text" + NL
     + "ELSE 'EntrezID:' || (annotation->>'entrez_id')::text END AS matched_term," + NL
+    + "'GENE // ' || gene_type || COALESCE(' // ' || (annotation->>'name')::text, '')" + NL
+    + "|| replace(CASE WHEN annotation->>'prev_symbol' IS NOT NULL OR annotation->>'alias symbol' IS NOT NULL THEN" + NL
+    + "COALESCE(' // Also Known As: ' || COALESCE((annotation->>'prev_symbol')::text , '')" + NL
+    + "|| CASE WHEN annotation->>'prev_symbol' IS NOT NULL AND annotation->>'alias_symbol' IS NOT NULL THEN ', ' ELSE '' END" + NL
+    + "|| COALESCE((annotation->>'alias_symbol')::text, ''))" + NL
+    + " || ' // Location: ' || (annotation->>'location')::text" + NL
+    + "ELSE '' END, '|', ', ') AS description," + NL
     + "'gene' AS record_type," + NL
     + "2 AS match_rank" + NL
     + "FROM CBIL.GeneAttributes WHERE source_id || ' ' ||  gene_symbol || ' ' || (annotation->>'entrez_id')::text || (annotation->>'name')::text" + NL 
@@ -44,6 +51,11 @@ public class SiteSearchService extends AbstractWdkService {
     + "CASE WHEN v.source_id IS NOT NULL THEN v.source_id" + NL
     + "WHEN length(split_part(v.record_pk, '_', 1)) > 30 THEN substr(split_part(v.record_pk, '_', 1), 0, 27) ELSE split_part(v.record_pk, '_', 1) END AS display," + NL
     + "replace(mv.term, '_', ':') AS matched_term," + NL
+    + "CASE WHEN v.is_adsp_variant THEN ' ADSP VARIANT' ELSE 'VARIANT' END" + NL
+    + "|| ' // ' || variant_class_abbrev" + NL
+    + "|| ' // Alleles: ' || display_allele" + NL
+    + "|| COALESCE(' // ' || v.most_severe_consequence, '')" + NL
+    + "|| COALESCE(' // ' || (v.annotation->'VEP_MS_CONSEQUENCE'->>'impact')::text, '') AS description," + NL
     + "'variant' AS record_type," + NL
     + "1 AS match_rank" + NL
     + "FROM NIAGADS.Variant v," + NL
@@ -56,6 +68,11 @@ public class SiteSearchService extends AbstractWdkService {
     + "CASE WHEN v.source_id IS NOT NULL THEN v.source_id" + NL
     + "WHEN length(split_part(v.record_pk, '_', 1)) > 30 THEN substr(split_part(v.record_pk, '_', 1), 0, 27) ELSE split_part(v.record_pk, '_', 1) END AS display," + NL
     + "replace(st.term, '_', ':') AS matched_term," + NL
+    + "CASE WHEN v.is_adsp_variant THEN ' ADSP VARIANT' ELSE 'VARIANT' END" + NL
+    + "|| ' // ' || variant_class_abbrev" + NL
+    + "|| ' // Alleles: ' || display_allele" + NL
+    + "|| COALESCE(' // ' || v.most_severe_consequence, '')" + NL
+    + "|| COALESCE(' // ' || (v.annotation->'VEP_MS_CONSEQUENCE'->>'impact')::text, '') AS description," + NL
     + "'variant' AS record_type," + NL
     + "1 AS match_rank" + NL
     + "FROM NIAGADS.Variant v, st" + NL
@@ -65,11 +82,13 @@ public class SiteSearchService extends AbstractWdkService {
     + "UNION ALL" + NL
     
     + "SELECT accession AS primary_key," + NL
-    + "name AS display," + NL
+    + "accession || ': ' || COALESCE(name || ' (' || attribution ||  ')', name) AS display," + NL
     + "CASE WHEN name @@ to_tsquery((SELECT term FROM st) || ':*') THEN name" + NL
     + "WHEN description @@ to_tsquery((SELECT term FROM st) || ':*') THEN description" + NL
     + "WHEN accession @@ to_tsquery((SELECT term FROM st) || ':*') THEN accession" + NL
     + "ELSE attribution END AS matched_term," + NL
+    + "'NIAGADS ACCESSION // ' || substr(description, 1, 130)" + NL
+    + "|| CASE WHEN length(description) <= 130 THEN '' ELSE '...' END AS description," + NL
     + "'dataset' AS record_type," + NL
     + "3 AS match_rank" + NL
     + "FROM NIAGADS.DatasetAttributes" + NL
@@ -78,15 +97,17 @@ public class SiteSearchService extends AbstractWdkService {
     + "UNION ALL" + NL
     
     + "SELECT track AS primary_key," + NL
-    + "name AS display," + NL
+    + "split_part(track, '_', 1) || ': ' || name || ' (' || attribution || ')' AS display," + NL
     + "CASE WHEN name @@ to_tsquery((SELECT term FROM st) || ':*') THEN name" + NL
     + "WHEN description @@ to_tsquery((SELECT term FROM st) || ':*') THEN description" + NL
-    + "WHEN track @@ to_tsquery((SELECT term FROM st) || ':*') THEN track" + NL
+    + "WHEN split_part(track, '_', 1) @@ to_tsquery((SELECT term FROM st) || ':*') THEN split_part(track, '_', 1)" + NL
     + "ELSE attribution END AS matched_term," + NL
+    + "'TRACK // ...' || replace(replace(replace(substr(description, 1, 130), 'summary statistics from ', ''), 'Summary statistics from ', ''), 'summary statistics for ', '')" + NL
+    + "|| CASE WHEN length(description) <= 130 THEN '' ELSE '...' END AS description," + NL
     + "'gwas_summary' AS record_type," + NL
     + "4 AS match_rank" + NL
     + "FROM NIAGADS.TrackAttributes" + NL
-    + "WHERE track || ' ' || name || ' ' || description || ' ' || attribution ILIKE '%' || (SELECT term FROM st) || '%'" + NL
+    + "WHERE split_part(track, '_', 1) || ' ' || name || ' ' || description || ' ' || attribution ILIKE '%' || (SELECT term FROM st) || '%'" + NL
     + "ORDER BY match_rank ASC, matched_term)" + NL
     
     + "SELECT jsonb_agg(matches)::text AS result" + NL
