2e743871c2bd40f90db9f83d9573ef86f86b7ab7
conor <conor.klamann@pennmedicine.upenn.edu>
2020-01-29 09:53:55 -0500

    show loading indicator if search results have not returned

--- Site/webapp/wdkCustomization/js/client/components/ResultsPage/ResultsPage.tsx
+++ Site/webapp/wdkCustomization/js/client/components/ResultsPage/ResultsPage.tsx
@@ -8,46 +8,57 @@ import {
   SearchResult,
   buildRouteFromResult
 } from "../AutoCompleteSearch/AutoCompleteSearch";
-import { isEmpty } from "lodash";
+import { chain, isEmpty, isObject, get, groupBy, mapValues } from "lodash";
 
-interface ResultsPage { }
+interface ResultsPage {}
 interface ResultsPageNavProps {
-  genes: number,
-  variants: number,
-  datasets: number,
-  accessions: number
+  genes: number;
+  variants: number;
+  datasets: number;
+  accessions: number;
 }
 
-const ResultsPageNav: React.FC<ResultsPageNavProps> =
-  ({ genes, variants, datasets, accessions }) => {
-
-    return (
-      <div>
-        <ul className="nav">
-          {genes > 0 &&
-            <li className="nav-item">
-              <a className="nav-link active" href="#genes">{genes} Gene{genes > 1 ? 's' : ''}</a>
-            </li>
-          }
-          {variants > 0 &&
-            <li className="nav-item">
-              <a className="nav-link active" href="#variants">{variants} Variant{variants > 1 ? 's' : ''}</a>
-            </li>
-          }
-          {accessions > 0 &&
-            <li className="nav-item">
-              <a className="nav-link active" href="#accessions">{accessions} NIAGADS Accession{accessions > 1 ? 's' : ''}</a>
-            </li>
-          }
-          {datasets > 0 &&
-            <li className="nav-item">
-              <a className="nav-link active" href="#datasets">{datasets} Summary Statistics Dataset{datasets > 1 ? 's' : ''}</a>
-            </li>
-          }
-        </ul>
-      </div>
-    );
-  };
+const ResultsPageNav: React.FC<ResultsPageNavProps> = ({
+  genes,
+  variants,
+  datasets,
+  accessions
+}) => {
+  return (
+    <div>
+      <ul className="nav">
+        {genes > 0 && (
+          <li className="nav-item">
+            <a className="nav-link active" href="#genes">
+              {genes} Gene{genes > 1 ? "s" : ""}
+            </a>
+          </li>
+        )}
+        {variants > 0 && (
+          <li className="nav-item">
+            <a className="nav-link active" href="#variants">
+              {variants} Variant{variants > 1 ? "s" : ""}
+            </a>
+          </li>
+        )}
+        {accessions > 0 && (
+          <li className="nav-item">
+            <a className="nav-link active" href="#accessions">
+              {accessions} NIAGADS Accession{accessions > 1 ? "s" : ""}
+            </a>
+          </li>
+        )}
+        {datasets > 0 && (
+          <li className="nav-item">
+            <a className="nav-link active" href="#datasets">
+              {datasets} Summary Statistics Dataset{datasets > 1 ? "s" : ""}
+            </a>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 const ResultsPage: React.FC<ResultsPage & RouteComponentProps<any>> = ({
   location
@@ -67,58 +78,71 @@ const ResultsPage: React.FC<ResultsPage & RouteComponentProps<any>> = ({
   const searchTerm = location.search.split("=")[1];
 
   //for now empty results are coming back as an empty object rather than array, so cover both possibilities
-  const resultsArray = isEmpty(results) ? [] : results;
-  const numGeneMatches = resultsArray.filter(function (value) { return value.record_type === 'gene' }).length;
-  const numDatasetMatches = resultsArray.filter(function (value) { return value.record_type === 'gwas_summary' }).length;
-  const numVariantMatches = resultsArray.filter(function (value) { return value.record_type === 'variant' }).length;
-  const numAccessionMatches = resultsArray.filter(function (value) { return value.record_type === 'dataset' }).length;
+  const resultsArray = isEmpty(results) ? [] : results,
+    counts = chain(resultsArray)
+      .groupBy("record_type")
+      .mapValues(v => v.length)
+      .value();
 
   //search term could change in header box
   useWdkEffect(sendRequest(searchTerm), [searchTerm]);
 
   return (
     <div>
-      {resultsArray.length ? (
-        <React.Fragment>
-          <h2>Results for search "{searchTerm}"</h2>
-          <ResultsPageNav genes={numGeneMatches} variants={numVariantMatches} datasets={numDatasetMatches} accessions={numAccessionMatches} />
-          <a id="gene" />
-          {resultsArray.map(res => _buildSearchResult(res, 'gene'))}
+      {isObject(results) ? (
+        resultsArray.length ? (
+          <React.Fragment>
+            <h2>Results for search "{searchTerm}"</h2>
+            <ResultsPageNav
+              genes={get(counts, "gene")}
+              variants={get(counts, "variant")}
+              datasets={get(counts, "gwas_summary")}
+              accessions={get(counts, "dataset")}
+            />
+            <a id="gene" />
+            {resultsArray.map(res => _buildSearchResult(res, "gene"))}
 
-          <a id="variant" />
-          {resultsArray.map(res => _buildSearchResult(res, 'variant'))}
+            <a id="variant" />
+            {resultsArray.map(res => _buildSearchResult(res, "variant"))}
 
-          <a id="accessions" />
-          {resultsArray.map(res => _buildSearchResult(res, 'dataset'))}
+            <a id="accessions" />
+            {resultsArray.map(res => _buildSearchResult(res, "dataset"))}
 
-          <a id="datasets" />
-          {resultsArray.map(res => _buildSearchResult(res, 'gwas_summary'))}
-        </React.Fragment>
-      ) : (
+            <a id="datasets" />
+            {resultsArray.map(res => _buildSearchResult(res, "gwas_summary"))}
+          </React.Fragment>
+        ) : (
           <h2>No results for search "{searchTerm}"</h2>
-        )}
+        )
+      ) : (
+        <h2>Loading results for {searchTerm}...</h2>
+      )}
     </div>
   );
 };
 
 const _buildSearchResult = (result: SearchResult, recordType: string) => {
   return (
-    result.record_type === recordType &&
-    <div key={result.primary_key}>
-      <hr />
-      <div>
-        <Link className="h6 wdk-Link" to={buildRouteFromResult(result)}>
-          {safeHtml(result.display)}
-        </Link>
-        {(result.record_type === 'variant' && result.matched_term.indexOf('merge') > -1) && <em>{result.matched_term}</em>}
+    result.record_type === recordType && (
+      <div key={result.primary_key}>
+        <hr />
+        <div>
+          <Link className="h6 wdk-Link" to={buildRouteFromResult(result)}>
+            {safeHtml(result.display)}
+          </Link>
+          {result.record_type === "variant" &&
+            result.matched_term.indexOf("merge") > -1 && (
+              <em>{result.matched_term}</em>
+            )}
+        </div>
+
+        <div>
+          <p className="site-search-result__description">
+            {safeHtml(result.description)}
+          </p>
+        </div>
       </div>
-
-
-      <div>
-        <p className="site-search-result__description">{safeHtml(result.description)}</p>
-      </div>
-    </div>
-
+    )
   );
 };
 
