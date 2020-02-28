f8a0df6d3cacd1f3f45477e2d19c94ead2104389
fossilfriend <egreenfest@gmail.com>
2020-02-28 09:56:03 -0500

    set initial params on question

--- Site/webapp/wdkCustomization/js/client/components/RecordPage/RecordHeading/DatasetRecord/GWASDatasetRecordHeading.tsx
+++ Site/webapp/wdkCustomization/js/client/components/RecordPage/RecordHeading/DatasetRecord/GWASDatasetRecordHeading.tsx
@@ -1,24 +1,17 @@
-import React, { useRef, useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { HeaderRecordActions } from './../Shared';
 import * as gr from './../../types';
 import { resolveJsonInput } from '../../../../util/jsonParse';
-
 import { HelpIcon } from 'wdk-client/Components';
-
-import WdkService, { useWdkEffect } from 'wdk-client/Service/WdkService';
 import { SubmissionMetadata, submitQuestion, updateParamValue, updateActiveQuestion } from 'wdk-client/Actions/QuestionActions';
-
-import { QuestionState, QuestionWithMappedParameters } from 'wdk-client/StoreModules/QuestionStoreModule';
-//import { useHistory } from 'react-router-dom';
+import { QuestionState } from 'wdk-client/StoreModules/QuestionStoreModule';
 
 
 const SEARCH_NAME = "gwas_stats";
-const ACCESSION_PARAM_NAME = "gwas_accession";
-const DATASET_PARAM_NAME = "gwas_dataset";
 const PVALUE_PARAM_NAME = "pvalue";
 
-interface StoreProps  {
+interface StoreProps {
 	externalUrls: { [key: string]: any },
 	webAppUrl: string;
 	questionState: QuestionState;
@@ -31,8 +24,6 @@ interface IRecordHeading {
 }
 
 interface SearchProps {
-	accession: string;
-	dataset: string;
 	questionState: QuestionState;
 }
 
@@ -42,7 +33,7 @@ type GWASDatasetRecord = StoreProps & gr.GWASDatasetRecord;
 const GWASDatasetSearchHelp: React.SFC<any> = props => {
 	return (
 		<div>
-			<p>Set the adjusted p-value threshold for GWAS significant. The search will return all genes supported by an 
+			<p>Set the adjusted p-value threshold for GWAS significant. The search will return all genes supported by an
 				p-value &le; the specified threshold.</p>
 			<p>p-values may be specified in decimal (e.g., 0.000003) or scientific (e.g., 3e-6 or 3^-6 or 3 x 10^-6) notation.</p>
 			<p>For exome array studies a p-value threshold of 1e-3 is recommended.</p>
@@ -53,7 +44,7 @@ const GWASDatasetSearchHelp: React.SFC<any> = props => {
 
 
 const GWASDatasetSearch: React.FC<SearchProps> = props => {
-	const { accession, dataset, questionState } = props;
+	const { questionState } = props;
 	//const [activeQuestion, setActionQuestion] = useState(null);
 
 	const submissionMetadata: SubmissionMetadata = { type: 'create-strategy' };
@@ -66,9 +57,10 @@ const GWASDatasetSearch: React.FC<SearchProps> = props => {
 		}, [SEARCH_NAME, submissionMetadata]
 	);
 
-	let handleChange = useCallback (
+	let handleChange = useCallback(
 		(event) => {
-			dispatch(updateParamValue({searchName: SEARCH_NAME,
+			dispatch(updateParamValue({
+				searchName: SEARCH_NAME,
 				parameter: questionState.question.parametersByName[PVALUE_PARAM_NAME],
 				paramValues: questionState.paramValues,
 				paramValue: event.target.value
@@ -78,12 +70,10 @@ const GWASDatasetSearch: React.FC<SearchProps> = props => {
 
 	return (
 		<form className="form-inline" onSubmit={handleSubmit}>
-			<input type="hidden" name={ACCESSION_PARAM_NAME} value={accession} />
-			<input type="hidden" name={DATASET_PARAM_NAME} value={dataset} />
-		
+
 			<div className="input-group mb-3">
 				<div className="input-group-prepend">
-					Mine this dataset 
+					Mine this dataset
 				</div>
 				<input
 					type="text"
@@ -104,17 +94,22 @@ const GWASDatasetSearch: React.FC<SearchProps> = props => {
 }
 
 const GWASDatasetRecordSummary: React.SFC<IRecordHeading & StoreProps> = props => {
-	const { record, recordClass, headerActions, externalUrls, questionState } = props;
+	const { record, recordClass, headerActions, questionState } = props;
 
 	const dispatch = useDispatch();
 	useEffect(() => {
+		const initialParamData = {
+			"gwas_accession": record.attributes.niagads_accession,
+			"gwas_dataset": record.displayName,
+			"pvalue": "5e-8"
+		};
 		dispatch(updateActiveQuestion({
-		  searchName: SEARCH_NAME,
-		  autoRun: false,
-		  initialParamData: undefined,
-		  stepId: undefined
+			searchName: SEARCH_NAME,
+			autoRun: false,
+			initialParamData: initialParamData,
+			stepId: undefined
 		}))
-	  }, []);
+	}, []);
 
 	return <React.Fragment>
 		<div className="record-summary-container dataset-record-summary-container">
@@ -130,22 +125,22 @@ const GWASDatasetRecordSummary: React.SFC<IRecordHeading & StoreProps> = props =
 				{record.attributes.is_adsp && <strong>&nbsp;{resolveJsonInput(record.attributes.is_adsp)}</strong>}
 			</h2>
 			<ul>
-
 				<li><h5 className="dataset-subtitle">{record.attributes.description}</h5></li>
 				<li><span className="label">Category:</span> {recordClass.displayName}</li>
 				<li><span className="label">Explore related datasets:</span>{resolveJsonInput(record.attributes.accession_link)} </li>
-				<GWASDatasetSearch  questionState={questionState} accession={record.attributes.niagads_accession} dataset={record.displayName} ></GWASDatasetSearch>
 			</ul>
+			<GWASDatasetSearch questionState={questionState} ></GWASDatasetSearch>
 		</div>
 	</React.Fragment>
 }
 
 
 const enhance = connect<StoreProps, any, IRecordHeading>(
-	(state: any) => ({ 
-		externalUrls: state.globalData.siteConfig.externalUrls, 
+	(state: any) => ({
+		externalUrls: state.globalData.siteConfig.externalUrls,
 		webAppUrl: state.globalData.siteConfig.webAppUrl,
-		questionState: (state.question.questions[SEARCH_NAME] || {}) as QuestionState, })
+		questionState: (state.question.questions[SEARCH_NAME] || {}) as QuestionState,
+	})
 );
 
 export default enhance(GWASDatasetRecordSummary);
