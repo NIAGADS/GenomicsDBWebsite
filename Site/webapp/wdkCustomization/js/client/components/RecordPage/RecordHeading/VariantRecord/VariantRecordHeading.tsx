fc21c5e28f7fbd03afefe496a8e0f66722810858
fossilfriend <egreenfest@gmail.com>
2020-03-09 09:48:07 -0400

    likely broken; will fix asap

--- Site/webapp/wdkCustomization/js/client/components/RecordPage/RecordHeading/VariantRecord/VariantRecordHeading.tsx
+++ Site/webapp/wdkCustomization/js/client/components/RecordPage/RecordHeading/VariantRecord/VariantRecordHeading.tsx
@@ -38,102 +38,124 @@ const VariantRecordSummary: React.SFC<gr.VariantRecordSummary & StoreProps> = pr
 				</div>
 				<hr />
 				<div className="col col-sm-3">
-								<div className="record-subtitle-container">
-					<h3 className="mb-2">
-						<strong>
-							{isJson(attributes.display_metaseq_id)
-								? resolveJsonInput(attributes.display_metaseq_id)
-								: attributes.display_metaseq_id}
-							{attributes.ref_snp_id && ' (' + attributes.ref_snp_id + ')'}
-						</strong>
-					</h3>
-					<p>
-						<strong>{sequence}</strong>
-					</p>
-					<p>
-						<strong>{attributes.variant_class}</strong>
-					</p>
-					{attributes.most_severe_consequence && <MostSevereConsequencesSection attributes={attributes} />}
-					{attributes.location && (
+					<div className="record-subtitle-container">
+						<h3 className="mb-2">
+							<strong>
+								{isJson(attributes.display_metaseq_id)
+									? resolveJsonInput(attributes.display_metaseq_id)
+									: attributes.display_metaseq_id}
+								{attributes.ref_snp_id && ' (' + attributes.ref_snp_id + ')'}
+							</strong>
+						</h3>
 						<p>
-							<span className="label">
-								<strong>Location:&nbsp;</strong>
-							</span>
-							{attributes.location}
+							<strong>{sequence}</strong>
 						</p>
-					)}
-					<p>
-						<span className="label">
-							<strong>Allele:&nbsp;</strong>
-						</span>
-						{attributes.display_allele}
-					</p>
-				</div>
-				<div className="record-detail-container">
-					<div className="reversed-info-container">
-						{isTrue(attributes.is_reversed) && (
+						<p>
+							<strong>{attributes.variant_class}</strong>
+						</p>
+						{attributes.most_severe_consequence && <MostSevereConsequencesSection attributes={attributes} />}
+						{attributes.location && (
 							<p>
-								This variant is on the
-								<Tooltip content="The variant is reported by dbSNP as being on the reverse strand. All sequences displayed in the GenomicsDB and mappings to variant annotations are on the forward strand.">
-									<span className="wdk-tooltip">&nbsp;reverse strand</span>
-								</Tooltip>
-								.
+								<span className="label">
+									<strong>Location:&nbsp;</strong>
+								</span>
+								{attributes.location}
 							</p>
 						)}
+						<p>
+							<span className="label">
+								<strong>Allele:&nbsp;</strong>
+							</span>
+							{attributes.display_allele}
+						</p>
 					</div>
-					{(attributes.adsp_wgs_qc_filter_status_display || attributes.adsp_wes_qc_filter_status_display) && (
-						<div className="adsp-variant-info-container">
-							<hr className="small" />
-							{attributes.is_adsp_variant && (
-								<strong>{resolveJsonInput(attributes.is_adsp_variant)}&nbsp;</strong>
+					<div className="record-detail-container">
+						<div className="reversed-info-container">
+							{isTrue(attributes.is_reversed) && (
+								<p>
+									This variant is on the
+								<Tooltip content="The variant is reported by dbSNP as being on the reverse strand. All sequences displayed in the GenomicsDB and mappings to variant annotations are on the forward strand.">
+										<span className="wdk-tooltip">&nbsp;reverse strand</span>
+									</Tooltip>
+									.
+							</p>
 							)}
-							{attributes.is_adsp_wes && resolveJsonInput(attributes.is_adsp_wes)}
-							{attributes.is_adsp_wgs && resolveJsonInput(attributes.is_adsp_wgs)}
-							<div className="qc-filters-container">
-								{attributes.adsp_wgs_qc_filter_status_display && (
-									<div>
-										<strong>WGS Filter Status</strong>:&nbsp;
-										{resolveJsonInput(attributes.adsp_wgs_qc_filter_status_display)}
-									</div>
-								)}
-								{attributes.adsp_wes_qc_filter_status_display && (
-									<div>
-										<strong>WES Filter Staus</strong>:&nbsp;
-										{resolveJsonInput(attributes.adsp_wes_qc_filter_status_display)}
-									</div>
-								)}
+						</div>
+						{attributes.alternative_variants && (
+							<div className="related-variants-container">
+								<AlternativeVariants altVars={attributes.alternative_variants} />
 							</div>
-							<hr className="small" />
-						</div>
-					)}
-					{attributes.alternative_variants && (
-						<div className="related-variants-container">
-							<AlternativeVariants altVars={attributes.alternative_variants} />
-						</div>
-					)}
-					{attributes.colocated_variants && (
-						<div className="colocated-variants-container">
-							<ColocatedVariants
-								position={attributes.position}
-								chromosome={attributes.chromosome}
-								colVars={attributes.colocated_variants}
-							/>
-						</div>
-					)}
-				</div>
+						)}
+						{attributes.colocated_variants && (
+							<div className="colocated-variants-container">
+								<ColocatedVariants
+									position={attributes.position}
+									chromosome={attributes.chromosome}
+									colVars={attributes.colocated_variants}
+								/>
+							</div>
+						)}
+					</div>
 				</div>
 				<div className="col">
-				{record.attributes.gws_datasets_summary_plot && (
-          <HighchartsTableTrellis 
-            data={JSON.parse(record.attributes.gws_datasets_summary_plot)}
-            properties={JSON.parse(getAttributeChartProperties(recordClass, "gws_datasets_summary_plot"))}
-          />
-        )}
+							<div className="header-summary-plot-title">
+								Has this variant been flagged by the ADSP?
+							</div>
+							{record.attributes.is_adsp_variant ? 
+							 <ADSPQCDisplay attributes={record.attributes} /> : <span className="none-adsp-variant">No</span>}
+
+					{record.attributes.gws_datasets_summary_plot && (
+						<div className="header-summary-plot-title">
+							To which AD-related dementias, neuropathologies, or biomarkers has this variant been linked through GWAS? 
+            &nbsp;&nbsp;&nbsp;<a href="#ad_variants_from_gwas">Browse the association evidence <i className="fa fa-level-down"></i></a> 
+						</div>
+						)}
+
+					{record.attributes.gws_datasets_summary_plot && (
+						<HighchartsTableTrellis
+							data={JSON.parse(record.attributes.gws_datasets_summary_plot)}
+							properties={JSON.parse(getAttributeChartProperties(recordClass, "gws_datasets_summary_plot"))}
+						/>
+					)}
 				</div>
 			</div>
 		</React.Fragment>
 	);
 };
+
+
+const ADSPQCDisplay: React.SFC<{attributes: gr.VariantRecordAttributes}> = props => {
+	const { attributes } = props;
+	return (
+		{(attributes.adsp_wgs_qc_filter_status_display || attributes.adsp_wes_qc_filter_status_display) && (
+			<div className="adsp-variant-info-container">
+				<hr className="small" />
+				{attributes.is_adsp_variant && (
+					<strong>{resolveJsonInput(attributes.is_adsp_variant)}&nbsp;</strong>
+				)}
+				{attributes.is_adsp_wes && resolveJsonInput(attributes.is_adsp_wes)}
+				{attributes.is_adsp_wgs && resolveJsonInput(attributes.is_adsp_wgs)}
+				<div className="qc-filters-container">
+					{attributes.adsp_wgs_qc_filter_status_display && (
+						<div>
+							<strong>WGS Filter Status</strong>:&nbsp;
+						{resolveJsonInput(attributes.adsp_wgs_qc_filter_status_display)}
+						</div>
+					)}
+					{attributes.adsp_wes_qc_filter_status_display && (
+						<div>
+							<strong>WES Filter Staus</strong>:&nbsp;
+						{resolveJsonInput(attributes.adsp_wes_qc_filter_status_display)}
+						</div>
+					)}
+				</div>
+				<hr className="small" />
+			</div>
+		)}
+
+
+	);
+}
 
 const AlternativeVariants: React.SFC<{ altVars: string }> = props => {
 	const vars = JSON.parse(props.altVars);
