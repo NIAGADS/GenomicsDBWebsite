5db100dc5d91ebb0e98248de7f59edc863593f1b
conor <conor.klamann@pennmedicine.upenn.edu>
2021-03-08 09:08:38 -0500

    update css imports

--- Site/webapp/wdkCustomization/js/client/main.ts
+++ Site/webapp/wdkCustomization/js/client/main.ts
@@ -4,15 +4,16 @@ import * as componentWrappers from "./component-wrappers";
 import { wrapRoutes } from "./routes";
 import pluginConfig from "./pluginConfig";
 
+//this needs to be imported first?
 import "eupathdb/wdkCustomization/css/client.scss";
-import "site/wdkCustomization/sass/client.scss";
+
+import "site/wdkCustomization/sass/client.scss"; //local;
 
 // Initialize the application.
 const context = initialize({
     componentWrappers,
     wrapRoutes,
     pluginConfig,
-    //storeWrappers,
 });
 
 //override default ebrc siteConfig with local
