ac1d25b1aad5b8fad3ccb2a5b68f51ba29b823c3
fossilfriend <egreenfest@gmail.com>
2021-03-16 11:41:12 -0400

    in progress but will compile

--- Service/src/main/java/org/niagads/genomics/service/services/Variant/VariantLookupService.java
+++ Service/src/main/java/org/niagads/genomics/service/services/Variant/VariantLookupService.java
@@ -21,7 +21,6 @@ import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkRuntimeException;
 import org.gusdb.wdk.service.service.AbstractWdkService;
 
-
 @Path("variant")
 public class VariantLookupService extends AbstractWdkService {
     private static final Logger LOG = Logger.getLogger(VariantLookupService.class);
@@ -29,70 +28,75 @@ public class VariantLookupService extends AbstractWdkService {
     private static final String VARIANT_PARAM = "id";
     private static final String FULL_VEP_PARAM = "full_vep";
 
-    private static final String VARIANT_DETAILS_SQL = "WITH searchTerms AS (SELECT unnest(string_to_array(?, ',')) AS variant_id)," + NL
-	+ "refSnps AS (SELECT r.search_term, v.record_pk FROM NIAGADS.Variant v," + NL
-        + "(SELECT variant_id AS search_term, find_variant_by_refsnp(variant_id) AS variant_id FROM searchTerms) r" + NL
-	+ "WHERE r.variant_id = v.variant_id)," + NL
-        + "marker AS (" +NL
-        + "SELECT variant_id AS search_term, find_variant_primary_key(variant_id) AS record_pk FROM searchTerms" + NL
-        + "UNION SELECT * FROM refSnps)," + NL
-        + "unmapped AS (select jsonb_agg(marker) AS result from marker where record_pk IS NULL OR record_pk LIKE 'rs%')," + NL
-        + "Details AS (" + NL
-        + "SELECT marker.search_term, v.record_pk AS variant, v.source_id AS ref_snp_id, v.metaseq_id," + NL 
-        + "v.variant_class_abbrev AS variant_class, v.source AS variant_source," + NL 
-        + "v.is_adsp_variant, v.is_adsp_wes, v.is_adsp_wgs, v.is_multi_allelic," + NL
-        + "v.most_severe_consequence," + NL
-        + "(v.annotation->'VEP_MS_CONSEQUENCE'->>'impact')::text AS most_severe_consequence_impact," + NL
-        + "(v.annotation->>'GWAS')::boolean AS is_associated_with_NIAGADS_dataset," + NL
-        + "(v.annotation->'VEP_MS_CONSEQUENCE'->>'gene_id')::text || ' // ' || (v.annotation->'VEP_MS_CONSEQUENCE'->>'gene_symbol')::text AS impacted_gene" + NL
-        + "FROM marker, NIAGADS.Variant v" + NL
-        + "WHERE v.record_pk = marker.record_pk)," + NL
-        + "Result AS (" + NL 
-        + "SELECT (jsonb_agg(details))::text AS result FROM details WHERE NOT EXISTS (SELECT result FROM unmapped WHERE result != NULL)" + NL
-        + "UNION" + NL
-        + "SELECT (jsonb_agg(details) || (select jsonb_agg(unmapped) FROM unmapped))::text AS result FROM details WHERE EXISTS (SELECT result FROM unmapped WHERE result != NULL))" + NL
-        + "SELECT * FROM result" + NL
-        + "WHERE result IS NOT NULL";
+    private static final String VARIANT_ID_CTE = "id AS (SELECT find_variant_primary_key(?) AS pk)";
 
+    private static final String VARIANT_DETAILS_CTE = "vdetails AS (" + NL + "SELECT id.pk AS variant_primary_key," + NL
+            + "split_part(pk, '_', 1) AS metaseq_id," + NL + "split_part(pk, '_', 2) AS ref_snp_id," + NL
+            + "'chr' || split_part(pk, ':', 1)::text AS chrm" + NL + "FROM id)";
 
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        // @OutSchema("niagads.variant.get-response")
-        public Response buildResponse(String body, @QueryParam(FULL_VEP_PARAM) Boolean fullVep, 
+    private static final String VARIANT_DETAILS_SQL = ""; /*
+                                                           * SELECT jsonb_build_object(" + NL 'chromosome',
+                                                           * v.chromosome," + NL 'search_term', 'rs1234'," + NL
+                                                           * 'location', v.location," + NL 'is_adsp_variant',
+                                                           * v.is_adsp_variant," + NL 'metaseq_id', v.metaseq_id," + NL
+                                                           * 'ref_snp_id', v.ref_snp_id," + NL 'allele_frequencies',
+                                                           * v.allele_frequencies," + NL 'cadd_scores', v.cadd_scores,"
+                                                           * + NL 'most_severe_consequence',
+                                                           * v.adsp_most_severe_consequence," + NL
+                                                           * 'transcript_consequences',
+                                                           * v.adsp_ranked_consequences->'transcript_consequences',
+                                                           * 'regulatory_feature_consequences',
+                                                           * v.adsp_ranked_consequences->'
+                                                           * regulatory_feature_consequences', 'motif_consequences',
+                                                           * v.adsp_ranked_consequences->'motif_feature_consequences',
+                                                           * 'intergenic_consequences',
+                                                           * v.adsp_ranked_consequences->'intergenic_consequences',
+                                                           * 'genomicsdb_flags', v.other_annotation->'GenomicsDB',
+                                                           * 'adsp_wgs_qc', v.other_annotation->'ADSP_WGS',
+                                                           * 'adsp_wes_qc', v.other_annotation->'ADSP_WES')::text AS
+                                                           * result FROM AnnotatedVDB.Variant v, vdetails d WHERE
+                                                           * left(v.metaseq_id, 50) = left(d.metaseq_id, 50) AND
+                                                           * v.ref_snp_id = d.ref_snp_id AND v.chromosome = d.chrm"
+                                                           */
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    // @OutSchema("niagads.variant.get-response")
+    public Response buildResponse(String body, @QueryParam(FULL_VEP_PARAM) Boolean fullVep,
             @QueryParam(VARIANT_PARAM) String variant) throws WdkModelException {
-                                          
-            LOG.info("Starting 'Variant Lookup' Service");
-            String response = "{}";
-            try {
 
-                response = fetchResult(variant);
-                if (response == null) {
-                    response = "{}";
-                }
+        LOG.info("Starting 'Variant Lookup' Service");
+        String response = "{}";
+        try {
+
+            response = fetchResult(variant);
+            if (response == null) {
+                response = "{}";
             }
-    
-            catch(WdkRuntimeException ex) {
-                throw new WdkModelException(ex);
-            }
-              
-            return Response.ok(response).build();
         }
 
-        private String fetchResult(String variant) {   
-    
-            WdkModel wdkModel = getWdkModel();
-            DataSource ds = wdkModel.getAppDb().getDataSource();
-            BasicResultSetHandler handler = new BasicResultSetHandler();
-            
-            //LOG.debug("Fetching details for variant:" + variant);
-
-            SQLRunner runner = new SQLRunner(ds, VARIANT_DETAILS_SQL, "variant-lookup-query");
-            runner.executeQuery(new Object[] {variant}, handler);
-           
-            List <Map <String, Object>> results = handler.getResults();
-	    if (results.isEmpty()) {
-		return null;
-	    }
-            return (String) results.get(0).get("result");
+        catch (WdkRuntimeException ex) {
+            throw new WdkModelException(ex);
         }
+
+        return Response.ok(response).build();
+    }
+
+    private String fetchResult(String variant) {
+
+        WdkModel wdkModel = getWdkModel();
+        DataSource ds = wdkModel.getAppDb().getDataSource();
+        BasicResultSetHandler handler = new BasicResultSetHandler();
+
+        // LOG.debug("Fetching details for variant:" + variant);
+
+        SQLRunner runner = new SQLRunner(ds, VARIANT_DETAILS_SQL, "variant-lookup-query");
+        runner.executeQuery(new Object[] { variant }, handler);
+
+        List<Map<String, Object>> results = handler.getResults();
+        if (results.isEmpty()) {
+            return null;
+        }
+        return (String) results.get(0).get("result");
+    }
 }
