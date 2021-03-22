d0ba809536dca8d17d32bd2a630cd977cf8029a8
fossilfriend <egreenfest@gmail.com>
2021-03-22 17:23:01 -0400

    override beta site banners

---
+++ patches/patch_EbrcWebsiteCommon.bash
@@ -0,0 +1,3 @@
+# Announcements
+echo "Beta site announcement text"
+cp $PROJECT_HOME/GenomicsDBWebsite/patches/Announcements.jsx $PROJECT_HOME/EbrcWebsiteCommon/Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
--- patches/patch_WDKClient.bash
+++ patches/patch_WDKClient.bash
@@ -1,3 +1,1 @@
-# Go Enrichment Result / this will likely break
-# echo "Replace hard-coded GO Enrichment Analysis result page; this will likely be fixed when code is moved from the 'MoveAfterRefactor' directory"
-# cp $PROJECT_HOME/NiagadsSite/patches/StepAnalysisGoEnrichmentResults.tsx $PROJECT_HOME/WDKClient/Client/src/Core/MoveAfterRefactor/Components/StepAnalysis/StepAnalysisGoEnrichmentResults.tsx
+# None
