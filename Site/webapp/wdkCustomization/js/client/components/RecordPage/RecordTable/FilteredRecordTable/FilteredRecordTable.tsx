28b3a9f831d54f860def5449af7375e2e04a51f3
fossilfriend <egreenfest@gmail.com>
2021-06-03 10:32:06 -0400

    new record-table in progress

--- Site/webapp/wdkCustomization/js/client/components/RecordPage/RecordTable/FilteredRecordTable/FilteredRecordTable.tsx
+++ Site/webapp/wdkCustomization/js/client/components/RecordPage/RecordTable/FilteredRecordTable/FilteredRecordTable.tsx
@@ -6,24 +6,11 @@ import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
-import Box from '@material-ui/core/Box'
 
 import { useTable, Column } from 'react-table'
+import { CustomRecordTableProps } from '../RecordTable/RecordTable';
 
-import * as rt from "../../types";
-
-export interface CustomRecordTableProps {
-    columns: Column<any>[];
-    data: any;
-}
-interface NiagadsRecordTable {
-    attributes: rt.TableAttribute[];
-    table: rt.Table;
-    data: { [key: string]: any }[];
-}
-
-
-const CustomRecordTable: React.FC<CustomRecordTableProps> = ({columns, data})  => {
+const FilteredRecordTable: React.FC<CustomRecordTableProps> = ({columns, data})  => {
   const { getTableProps, headerGroups, rows, prepareRow } = useTable({
     columns,
     data,
@@ -63,27 +50,4 @@ const CustomRecordTable: React.FC<CustomRecordTableProps> = ({columns, data})  =
   )
 }
 
-
-const _buildColumn = (attribute: rt.TableAttribute, sortable: boolean, filterType?: any) => ({
-    Header: _buildHeader(attribute),
-    sortable,
-    accessor: resolveAccessor(attribute.name, attribute),
-    id: attribute.name,
-    filterMethod: filterType ? filterType : (filter: any, rows: any) => rows,
-});
-
-const _buildHeader = (attribute: rt.TableAttribute) => {
-    return (
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-            <Box display="flex">
-                <Box overflow="hidden" textOverflow="ellipsis" paddingRight="2px">
-                    {attribute.displayName}
-                </Box>
-                {attribute.help ? withTooltip(<span className="fa fa-question-circle-o" />, attribute.help) : null}
-            </Box>
-            {attribute.isSortable && <SortIconGroup />}
-        </Box>
-    );
-};
-
-export default CustomRecordTable;
+export default FilteredRecordTable;
