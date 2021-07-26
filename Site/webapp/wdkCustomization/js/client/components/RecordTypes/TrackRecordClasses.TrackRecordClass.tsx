552c5ed5d07c248925a3df735b1cedec243f5f50
fossilfriend <egreenfest@gmail.com>
2021-07-25 23:40:01 -0400

    tracks

---
+++ Site/webapp/wdkCustomization/js/client/components/RecordTypes/TrackRecordClasses.TrackRecordClass.tsx
@@ -0,0 +1,4 @@
+import RecordHeading from "../RecordPage/RecordHeading/DatasetRecord/GWASDatasetRecordHeading";
+import RecordMainSection from "../RecordPage/RecordMainSection/RecordMainSection";
+
+export { RecordHeading, RecordMainSection };
