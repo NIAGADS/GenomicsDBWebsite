ed6d9b9d759de2722e919ff579e1cb34b8f9b868
fossilfriend <egreenfest@gmail.com>
2021-11-08 21:26:59 -0500

    data dictionary record

---
+++ Site/webapp/wdkCustomization/js/client/components/Record/Page/Heading/OntologyRecord/OntologyRecordHeading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import { HeaderRecordActions } from "../Shared";
+import { RecordHeading } from "../RecordHeadingTypes";
+import { Grid } from "@material-ui/core";
+import { BaseText, Heading, Subheading } from "../../../../MaterialUI";
+import { RecordInstance, RecordClass } from 'wdk-client/Utils/WdkModel';
+import { DefaultBackgroundPanel } from "../../../../MaterialUI";
+
+
+const OntologyRecordSummary: React.FC<RecordHeading> = ({ record, recordClass, headerActions }) => (
+    <DefaultBackgroundPanel hasBaseArrow={false}>
+        <Heading>{record.attributes.name}</Heading>
+        <Subheading>{record.attributes.description}</Subheading>
+    </DefaultBackgroundPanel>
+);
+
+export default OntologyRecordSummary;
