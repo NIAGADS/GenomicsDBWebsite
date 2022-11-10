d63a665c7d3b37e1f0fcfe2786c0df85ab740b13
fossilfriend <egreenfest@gmail.com>
2022-11-10 18:13:16 +0000

    table prop json

---
+++ Site/webapp/wdkCustomization/js/client/data/record_properties/_trackRecordProperties.ts
@@ -0,0 +1,25 @@
+import { RecordTableProperties } from "./_recordTableProperties";
+
+export const _trackTableProperties: { [name: string]: RecordTableProperties } = {
+    top_variants: {
+        filters: {
+            pvalue: "pvalue",
+            chromosome: "pie",
+            adsp_variant_flag: "pie",
+            most_severe_consequence: "pie",
+            msc_impact: "pie",
+            msc_is_coding_flag: "pie"
+        },
+        filterGroups: {
+            Statistics: ["pvalue"],
+            Position: ["chromosome"],
+            Annotation: ["adsp_variant_flag", "most_severe_consequence", "msc_impact", "msc_is_coding_flag"],
+        },
+        defaultFilter: "pvalue",
+        requiredColumns: ["track", "variant", "pvalue"],
+        defaultOpen: true,
+        canFilter: true,
+        canToggleColumns: true,
+        sortedBy: [{ id: "pvalue", descending: false }],
+    },
+};
