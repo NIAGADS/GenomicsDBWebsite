357b9c4baeb24895107a5b7a4aee06313a3c8c48
fossilfriend <egreenfest@gmail.com>
2022-11-11 13:11:32 -0500

    threshold filter for pvalue

--- Site/webapp/wdkCustomization/js/client/components/Record/RecordTable/RecordTableFilters/PValueFilter/index.ts
+++ Site/webapp/wdkCustomization/js/client/components/Record/RecordTable/RecordTableFilters/PValueFilter/index.ts
@@ -1,6 +1,6 @@
 
 import { negLog10p, getMinMaxNegLog10PValue, invertNegLog10p } from "../filters/negLog10pFilter";
-export const DEFAULT_FILTER_VALUE = negLog10p(5e-8);
+export const DEFAULT_FILTER_VALUE = 5e-8;
 
 export * from './PValueFilter';
 export * from './PVaueSliderFilter';
