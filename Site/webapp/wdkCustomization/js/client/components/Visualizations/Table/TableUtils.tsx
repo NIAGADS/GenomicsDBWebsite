fe566f8b8ea2ed4c3781610c8d4bc2fd83ab7385
fossilfriend <egreenfest@gmail.com>
2022-11-18 18:21:48 +0000

    missing checks

--- Site/webapp/wdkCustomization/js/client/components/Visualizations/Table/TableUtils.tsx
+++ Site/webapp/wdkCustomization/js/client/components/Visualizations/Table/TableUtils.tsx
@@ -1,11 +1,17 @@
-import { isString, isObject,get } from 'lodash'
+import { isString, isObject, get, toNumber } from 'lodash'
 
 export const parseFieldValue = (value: any): any => {
     return isString(value) || !value
     ? value
     : get(value, "props.dangerouslySetInnerHTML.__html")
     ? value.props.dangerouslySetInnerHTML.__html
-    : isObject(value)
-    ? parseFieldValue(value)
+    : value.type && value.type.name === "CssBarChart"
+    ? toNumber(value.props.original)
+    : isObject(value) && (value as { displayText: string }).displayText
+    ? (value as { displayText: string }).displayText
+    : value.value
+    ? value.value
+    : value.props && value.props.children
+    ? parseFieldValue(value.props.children)
     : "";
 };
