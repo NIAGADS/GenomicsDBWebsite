45335ae3f1e507156249a5242819f6387e7e9188
fossilfriend <egreenfest@gmail.com>
2022-11-18 16:56:07 +0000

    switch

--- Site/webapp/wdkCustomization/js/client/components/Record/RecordTable/RecordTableUtils.tsx
+++ Site/webapp/wdkCustomization/js/client/components/Record/RecordTable/RecordTableUtils.tsx
@@ -6,7 +6,7 @@ import CssBarChart from "./Columns/CssBarChart";
 
 const _parseJson = (value: any) => {
     //not reallly a json test, more like a check to see if the backend is sending us something we assume we can treat as json
-    if (!value) return 'n/a';
+    if (!value) return "n/a";
     if (!isString(value)) return value;
     if (value.startsWith("[") || value.startsWith("[{") || value.startsWith("{")) {
         try {
@@ -21,7 +21,7 @@ const _parseJson = (value: any) => {
 export const resolveData = (data: { [key: string]: any }[]): { [key: string]: any }[] => {
     return data.map((datum) => {
         return forIn(datum, (v: string, k: string, o: { [x: string]: any }) => {
-            o[k] = k.endsWith("_flag") && !v ? null : _parseJson(v); 
+            o[k] = k.endsWith("_flag") && !v ? null : _parseJson(v);
         });
     });
 };
@@ -39,11 +39,14 @@ export const resolveNAs = (value: string, accessor?: any) => {
 
 export const resolveAccessor = (key: string, accessorType: ColumnAccessorType = "Default") => {
     switch (accessorType) {
-        case "Default":
-            return (row: any) => (isObject(row[key]) ? resolveObjectInput(row[key]) : resolveNAs(row[key]));
         case "StackedBar":
             return (row: any) => <CssBarChart value={row[key]} percentage={row[key] * 100} />;
         case "BooleanFlag":
             return (row: any) => (isObject(row[key]) ? resolveObjectInput(row[key]) : row[key]);
+        case "Link":
+        case "ScientificNotation":
+        case "Float":
+        case "Default":
+            return (row: any) => (isObject(row[key]) ? resolveObjectInput(row[key]) : resolveNAs(row[key]));
     }
 };
