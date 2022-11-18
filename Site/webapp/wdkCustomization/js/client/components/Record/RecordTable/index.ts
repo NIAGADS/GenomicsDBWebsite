dc0eb5f648683469a754d62f52bb4c9e1326b3a4
fossilfriend <egreenfest@gmail.com>
2022-11-18 16:08:43 +0000

    remove empty sort file

--- Site/webapp/wdkCustomization/js/client/components/Record/RecordTable/index.ts
+++ Site/webapp/wdkCustomization/js/client/components/Record/RecordTable/index.ts
@@ -1,6 +1,5 @@
 export * from "./RecordTableFilters";
 export * from "./RecordTable";
-export * from "./RecordTableSort";
 export * from "./RecordTableTypes";
 export * from "./RecordTableUtils";
 export * from "./Columns";
