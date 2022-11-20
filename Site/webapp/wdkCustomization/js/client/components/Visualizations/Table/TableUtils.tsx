105a93cd7530636c7b865c60648e8ec6bd111905
fossilfriend <egreenfest@gmail.com>
2022-11-20 01:31:36 -0500

    nulls always at end

--- Site/webapp/wdkCustomization/js/client/components/Visualizations/Table/TableUtils.tsx
+++ Site/webapp/wdkCustomization/js/client/components/Visualizations/Table/TableUtils.tsx
@@ -1,19 +1,19 @@
 import { isString, isObject, get, toNumber } from 'lodash'
 
-export const parseFieldValue = (value: any): any => {
+export const parseFieldValue = (value: any, returnNA:boolean=false): any => {
     return isString(value) || !value
     ? value
     : get(value, "props.dangerouslySetInnerHTML.__html")
     ? value.props.dangerouslySetInnerHTML.__html
-    : value.type && value.type.name === "CssBarChart"
-    ? toNumber(value.props.original)
     : isObject(value) && (value as { displayText: string }).displayText
     ? (value as { displayText: string }).displayText
     : value.value
     ? value.value
     : value.props && value.props.children
-    ? parseFieldValue(value.props.children)
+    ? parseFieldValue(value.props.children, returnNA)
     : value.props 
-    ? parseFieldValue(value.props)
+    ? parseFieldValue(value.props, returnNA)
+    : returnNA 
+    ? "n/a" 
     : "";
 };
