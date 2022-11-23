b372f9aecaedc70d8e6e59d0d75fa20a84c4529e
fossilfriend <egreenfest@gmail.com>
2022-11-23 12:41:43 -0500

    attributes

--- Site/webapp/wdkCustomization/js/client/components/Record/Attributes/RelativePositionSpan.tsx
+++ Site/webapp/wdkCustomization/js/client/components/Record/Attributes/RelativePositionSpan.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import { ColumnAccessor, ColoredSpan } from "@viz/Table/ColumnAccessors";
+import { ColumnAccessor, ColoredTextAccessor } from "@viz/Table/ColumnAccessors";
 
 export const RelativePositionSpan: React.SFC<ColumnAccessor> = ({ value }) => {
     const className = value.includes("in") || value.includes("overlap") ? "colocated" : value.toLowerCase();
 
-    return <ColoredSpan value={value} className={`position-indicator ${className}`} />;
+    return <ColoredTextAccessor value={value} className={`position-indicator ${className}`} />;
 };
--- Site/webapp/wdkCustomization/js/client/components/Record/Attributes/index.ts
+++ Site/webapp/wdkCustomization/js/client/components/Record/Attributes/index.ts
@@ -2,3 +2,5 @@ export * from "./LabeledAttributeItem";
 export * from "./GeneRecordAttributes";
 export * from "./RelativePositionSpan";
 export * from "./VariantConsequenceImpactSpan";
+export * from "./BooleanAttribute";
+export * from "./LinkAttributes";
