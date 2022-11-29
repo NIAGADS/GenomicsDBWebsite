60b305242be6fc09ab028ddcbc8e36607a14a113
fossilfriend <egreenfest@gmail.com>
2022-11-29 07:24:57 -0500

    alerts

--- Site/webapp/wdkCustomization/js/client/components/Visualizations/Table/TableFilters/ColumnFilters/ZeroFilterChoicesMsg.tsx
+++ Site/webapp/wdkCustomization/js/client/components/Visualizations/Table/TableFilters/ColumnFilters/ZeroFilterChoicesMsg.tsx
@@ -1,26 +1,9 @@
 import React from "react";
-import Alert from "@material-ui/lab/Alert";
-import AlertTitle from "@material-ui/lab/AlertTitle";
-import { makeStyles } from "@material-ui/core/styles";
+import { InfoAlert } from "@components/MaterialUI"
 
-
-const useStyles = makeStyles((theme) => ({
-    alert: {
-      fontSize: "12px",
-      marginBottom: theme.spacing(1)
-    },
-    alertTitle: {
-        fontWeight: "bold",
-        fontSize: "14px"
-    }
-  }));
 
 export const ZeroFilterChoicesMsg: React.SFC<{ label: string }> = ({ label }) => {
-    const classes = useStyles();
     return (
-        <Alert severity="info" className={classes.alert}>
-            <AlertTitle className={classes.alertTitle}>{label}</AlertTitle>
-            No choices (or only NAs) available with selected filters.
-        </Alert>
+        <InfoAlert title={label} message="No choices (or only NAs) available with selected filters." />    
     );
 };
