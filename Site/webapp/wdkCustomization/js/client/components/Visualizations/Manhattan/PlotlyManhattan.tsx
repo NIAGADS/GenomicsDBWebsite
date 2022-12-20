b218877e30ce5c3361efa2802ddffbb7a7be52e1
fossilfriend <egreenfest@gmail.com>
2022-12-20 20:03:10 +0000

    placehlder plotly component

---
+++ Site/webapp/wdkCustomization/js/client/components/Visualizations/Manhattan/PlotlyManhattan.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Plot from 'react-plotly';
+
+interface ManhattanPlot {
+    track: string;
+}
+
+export const PlotlyManhattan: React.SFC<ManhattanPlot> = ({ track }) => {
+
+    
+    return (null);
+}
