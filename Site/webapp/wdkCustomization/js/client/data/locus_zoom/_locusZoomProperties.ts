af9187d2c9330bc1bc3923a049e83894b6c5cd30
fossilfriend <egreenfest@gmail.com>
2023-03-03 15:11:50 +0000

    ld population menu contents

---
+++ Site/webapp/wdkCustomization/js/client/data/locus_zoom/_locusZoomProperties.ts
@@ -0,0 +1,8 @@
+export const _ldPopulationChoices = [
+    { display_name: "ADSP 17K", value: "ADSP" },
+    /*{ display_name: 'AFR', value: 'AFR' },
+        { display_name: 'AMR', value: 'AMR' },
+        { display_name: 'EAS', value: 'EAS' },
+        { display_name: 'EUR', value: 'EUR' },
+        { display_name: 'SAS', value: 'SAS' }, */
+];
