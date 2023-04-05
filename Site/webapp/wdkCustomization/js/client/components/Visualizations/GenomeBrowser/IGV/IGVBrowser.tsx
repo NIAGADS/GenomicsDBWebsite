d876c2f00980e985dd1942d4a562370525c82d53
fossilfriend <egreenfest@gmail.com>
2023-04-05 11:50:51 +0000

    gene popups - in progress

--- Site/webapp/wdkCustomization/js/client/components/Visualizations/GenomeBrowser/IGV/IGVBrowser.tsx
+++ Site/webapp/wdkCustomization/js/client/components/Visualizations/GenomeBrowser/IGV/IGVBrowser.tsx
@@ -3,6 +3,7 @@ import igv from "igv/dist/igv.esm";
 import { noop, merge, get } from "lodash";
 import { GWASServiceTrack as GWASTrack, VariantServiceTrack as VariantTrack } from "@viz/GenomeBrowser";
 import { makeStyles, createStyles, Theme } from "@material-ui/core";
+import { analysisBaseTabConfigs } from "wdk-client/Core/MoveAfterRefactor/StoreModules/StepAnalysis/StepAnalysisSelectors";
 
 const HASH_PREFIX = "#/locus/";
 const ALWAYS_ON_TRACKS = ["ideogram", "ruler", "sequence", "REFSEQ_GENE"];
@@ -48,22 +49,93 @@ export const removeTrackById = (trackId: string, browser: any) => {
     browser.removeTrack(trackView[0].track);
 };
 
-export const IGVBrowser: React.FC<IGVBrowser> = ({ locus, webAppUrl, searchUrl, options, onBrowserLoad, onTrackRemoved }) => {
+export const IGVBrowser: React.FC<IGVBrowser> = ({
+    locus,
+    webAppUrl,
+    searchUrl,
+    options,
+    onBrowserLoad,
+    onTrackRemoved,
+}) => {
     const classes = useStyles();
     const tableClass = classes.popupTable.toString();
 
+    const _geneSubFeaturePopoverData = (fields: string[], info: any, pData: any[]) => {
+        // type
+        // name
+        // biotype - most
+        // ends with _id    / some may have transcript or exon id plus another id
+        // location
+        // number -- catch outside
+        pData.push({ name: "Feature:", value: info[fields.indexOf("type")].value });
+        pData.push({ name: "Name:", value: info[fields.indexOf("name")].value });
+        if (fields.includes("biotype")) {
+            pData.push({ name: "Biotype:", value: info[fields.indexOf("biotype")].value.replace(/_/g, " ") });
+        }
+
+        // find IDs for this feature
+        // next feature starts at the next <hr>
+        // so stop looking when field === "hr"
+        const idIndices = fields
+            .slice(0, fields.indexOf("hr"))
+            .map((elem: string, index: number) => (elem.endsWith("id") ? index : ""))
+            .filter(String);
+
+        idIndices.forEach((index: any) => {
+            let idName = info[index].name;
+            idName = idName.includes("_")
+                ? idName.charAt(0).toUpperCase() + idName.slice(1).replace("_id", " ID:")
+                : idName.toUpperCase().replace("ID", " ID:");
+
+            pData.push({ name: idName, value: info[index].value });
+        });
+
+        pData.push({ name: "Location: ", value: info[fields.indexOf("location")].value });
+
+        return pData;
+    };
+
     const _geneTrackPopoverData = (info: any) => {
-        const fields = info.map((elem: any, index: number) => elem.name.toLowerCase().replace(':', '').replace(' ', '_'));
+        const regexp = / \[.+\]/i;
+
+        let fields = info.map((elem: any) => {
+            return elem.hasOwnProperty("name") ? elem.name.toLowerCase().replace(":", "").replace(" ", "_") : "hr";
+        });
         let pData = [];
 
-        const geneId = info[fields.indexOf('gene_id')];
-        const geneSymbol = info[fields.indexOf('name')]
-        const recHref = webAppUrl + "/record/gene/";
-        //let value = '<a href="' + recHref + gene
-        //pData.push({name: "Gene", html})
-        // find first location and after that, take everything in a list of selected fields
-        return info;
-    }
+        const geneId = info[fields.indexOf("gene_id")].value;
+        const geneSymbol = info[fields.indexOf("name")].value;
+        const product = info[fields.indexOf("description")].value.replace(regexp, "");
+        const recHref = webAppUrl + "/app/record/gene/" + geneId;
+        pData.push({ name: "Feature:", value: "gene" });
+        pData.push({ name: "Name:", value: geneSymbol });
+        pData.push({
+            name: "More Info:",
+            html: '<a target="_blank" href="' + recHref + '" title=""/>',
+            title: "View GenomicsDB report for gene " + geneSymbol,
+        });
+        pData.push({ name: "Product:", value: product });
+        pData.push({ name: "Biotype:", value: info[fields.indexOf("biotype")].value.replace(/_/g, " ") });
+        pData.push({ name: "Location:", value: info[fields.indexOf("location")].value });
+
+        // find first divider and take everything from that point on
+        let infoSlice = info;
+        while (fields.includes("hr")) {
+            const dividerIndex = fields.indexOf("hr"); // find next divider
+            pData.push("<hr>"); // push the divider
+            fields = fields.splice(dividerIndex + 1);
+            infoSlice = infoSlice.splice(dividerIndex + 1);
+            if (fields.length > 1) {
+                pData = _geneSubFeaturePopoverData(fields, infoSlice, pData);
+            } else {
+                // should be Exon Number
+                if (fields[0] === "exon_number") {
+                    pData.push({ name: "Exon Number:", value: infoSlice[0].value });
+                }
+            }
+        }
+        return pData;
+    };
 
     const _customTrackPopup = (track: any, popoverData: any) => {
         // Don't show a pop-over when there's no data.
@@ -71,35 +143,36 @@ export const IGVBrowser: React.FC<IGVBrowser> = ({ locus, webAppUrl, searchUrl, 
             return false;
         }
 
-        popoverData = (track.id === "ENSEMBL_GENE" ? _geneTrackPopoverData(popoverData) : popoverData);
+        popoverData = track.id === "ENSEMBL_GENE" ? _geneTrackPopoverData(popoverData) : popoverData;
 
         const tableStartMarkup = '<table class="' + tableClass + '">';
         let markup = tableStartMarkup;
 
         popoverData.forEach(function (item: any) {
-            if (item === "<hr>" || item === "<HR>") {
-                markup += "</table> " + item + " " + tableStartMarkup;
-            }
-
-            let value = item.html ? item.html : item.value ? item.value : item.toString();
-            let color = item.color ? item.color : null;
-
-            if (color !== null) {
-                value = '<span style="padding-left: 8px; border-left: 4px solid ' + color + '">' + value + "</span>";
-            }
-
-            const title = item.title ? item.title : null;
-            const label = item.name ? item.name : null;
-
-            if (title) {
-                value = `<div title="${title}">${value}</div>`;
-            }
-
-            if (label) {
-                markup += "<tr><td>" + label + "</td><td>" + value + "</td></tr>";
+            if (item === '<hr>' || item === '<hr/>' || item === '<HR>' || item === '<HR/>') {
+                markup += "</table>" + " <hr> " + tableStartMarkup;
             } else {
-                // not a name/value pair
-                markup += "<tr><td>" + value + "</td></tr>";
+                let value = item.html ? item.html : item.value ? item.value : item.toString();
+                let color = item.color ? item.color : null;
+
+                if (color !== null) {
+                    value =
+                        '<span style="padding-left: 8px; border-left: 4px solid ' + color + '">' + value + "</span>";
+                }
+
+                const title = item.title ? item.title : null;
+                const label = item.name ? item.name : null;
+
+                if (title) {
+                    value = `<div title="${title}">${value}</div>`;
+                }
+
+                if (label) {
+                    markup += "<tr><td>" + label + "</td><td>" + value + "</td></tr>";
+                } else {
+                    // not a name/value pair
+                    markup += "<tr><td>" + value + "</td></tr>";
+                }
             }
         });
 
